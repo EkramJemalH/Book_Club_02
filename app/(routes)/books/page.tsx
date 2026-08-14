fff8542968b25fd2533ba23307f98de4f83d25f5
EkramJemalH <ekramjemalh@gmail.com>
2026-08-14 07:43:17 +0300

    refactor: restructure books page and fallback data

--- app/(routes)/books/page.tsx
+++ app/(routes)/books/page.tsx
@@ -1,4 +1,4 @@
-import BooksPage from '@/app/components/books/page'
+import BooksPage from '@/app/components/books/BookPage'
 import globalStyles from '@/app/globals.css'
 
 export default function BooksRoutePage() {
